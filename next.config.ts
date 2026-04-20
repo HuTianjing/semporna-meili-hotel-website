7a98e7200154bed6785100e72dde8682e2829b0d
叶颖 <140698233+JiayiLuoJarvis@users.noreply.github.com>
2026-04-20 21:00:37 +0800

    设置基础路径为 /v2，以支持多版本路由架构

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
 const nextConfig: NextConfig = {
+  basePath: '/v2',
   output: 'standalone',
   reactStrictMode: true,
   images: {
