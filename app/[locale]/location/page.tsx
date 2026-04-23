a7d15f44332a1568846f2da82ead640000ed2a1d
叶颖 <140698233+JiayiLuoJarvis@users.noreply.github.com>
2026-04-23 11:49:41 +0800

    refactor: remove LocationSchedule component and update localization files with new content for Location page, including navigation and cultural information

--- app/[locale]/location/page.tsx
+++ app/[locale]/location/page.tsx
@@ -1,15 +1,9 @@
-﻿import LocationGeo from '@/components/location/LocationGeo';
-import LocationRoute from '@/components/location/LocationRoute';
-import LocationSchedule from '@/components/location/LocationSchedule';
-import LocationChecklist from '@/components/location/LocationChecklist';
+﻿
 
 export default function LocationPage() {
   return (
     <main className="w-full flex-1">
-      <LocationGeo />
-      <LocationRoute />
-      <LocationSchedule />
-      <LocationChecklist />
+
     </main>
   );
 }
